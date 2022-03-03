da32e34a1cb6132f120ba11c1b25a325d643c667
mohsenzolfagharian <mohsen.zolfagharian.98@gmail.com>
2022-03-03 22:02:07 +0330

    its better to be Modular Code :)

--- run_leage.py
+++ run_leage.py
@@ -14,15 +14,3 @@ for x in tuple_teams_list:
 list_teams = list(teams.items())
 for x in range(0, 2):
     my_league.face2face_mathces(list_teams)
-# for x in range(len(list_teams)):
-#     c = 1
-#     # inja bayad facae 2 face ijad konim :)
-#     while c <= len(list_teams):
-#         try:
-#             goals = my_league.goals_number()
-#             reslut_match = my_league.match_result(goals, list_teams[x][1].power_team(), list_teams[x+c][1].power_team(),
-#                                                   list_teams[x][1].name_team(), list_teams[x+c][1].name_team())
-#             print(reslut_match)
-#             c += 1
-#         except IndexError:
-#             break

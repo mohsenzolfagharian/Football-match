932a6a2ca54cd5ff972d7a6cf298a9136b35c0d7
mohsenzolfagharian <mohsen.zolfagharian.98@gmail.com>
2022-04-02 20:21:13 +0430

    updated DB fuctions to make update for a wanted column

--- data_base.py
+++ data_base.py
@@ -14,8 +14,9 @@ class DB:
 
     def insert_teams(self, table_name, team_name):
         cur = conn.cursor()
-        cur.execute(f"INSERT INTO league1 (teams) VALUES('{team_name}');")
+        cur.execute(f"INSERT INTO {table_name} (teams) VALUES('{team_name}');")
         conn.commit()
+        cur.close()
         conn.close()
 
     def delete_table(self, table_name):
@@ -23,14 +24,42 @@ class DB:
         cur.execute(f"DROP TABLE {table_name};")
         cur.close()
 
-    def add_data(self, table_name, team, ga=None, gf=None, win=None, draw=None, lose=None):
+    def delete_team(self, table_name, team):
         cur = conn.cursor()
-        cur.execute(f"UPDATE {table_name} SET ga={ga}, gf={gf} WHERE teams='{team}';")
+        cur.execute(f"DELETE FROM {table_name} WHERE teams={team};")
         conn.commit()
         conn.close()
+
+    def update_info(self, table_name, team, col, value):
+        cur = conn.cursor()
+        cur.execute(f"UPDATE {table_name} SET {col}={value} WHERE teams='{team}';")
+        conn.commit()
+        conn.close()
+
+    def show_all(self, table_name):
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM {table_name};")
+        data = cur.fetchall()
+        conn.close()
+        return data
+    
+
 
 
 a = DB()
 # a.Create_table(1)
+# a.update_info('league1', 'manchester', 'gf', 10)
 # a.delete_table('league1')
-a.insert_teams('league1', 'arsenal')
+
+# a.insert_teams('league1', 'real')
+
+# a.delete_team("league1", "arsenal")
+
+# a.add_gf('league1', 'manchester', 5)
+
+
+# data = a.show_all('league1')
+# for d in data:
+#     print(d)
+
+

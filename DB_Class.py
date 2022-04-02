9001441b11ccd59acc5f10ebc861025987fc2d59
mohsenzolfagharian <mohsen.zolfagharian.98@gmail.com>
2022-04-03 01:01:35 +0430

    teams are added to DB :)

--- DB_Class.py
+++ DB_Class.py
@@ -1,52 +1,52 @@
 import sqlite3
 
-conn = sqlite3.connect('table_league')
 
 
 class DB:
     def __init__(self):
-        pass
+        self.conn = sqlite3.connect('table_league')
+
 
     def Create_table(self, season):
-        cur = conn.cursor()
+        cur = self.conn.cursor()
         cur.execute(f"CREATE TABLE league{season} (teams TEXT, ga INT, gf INT, win INT, draw INT, lose INT);")
-        cur.close()
+        # cur.close()
 
     def insert_teams(self, table_name, team_name):
-        cur = conn.cursor()
+        cur = self.conn.cursor()
         cur.execute(f"INSERT INTO {table_name} (teams) VALUES('{team_name}');")
-        conn.commit()
-        cur.close()
-        conn.close()
+        self.conn.commit()
+        # cur.close()
+        # self.conn.close()
 
     def delete_table(self, table_name):
-        cur = conn.cursor()
+        cur = self.conn.cursor()
         cur.execute(f"DROP TABLE {table_name};")
-        cur.close()
+        # cur.close()
 
     def delete_team(self, table_name, team):
-        cur = conn.cursor()
+        cur = self.conn.cursor()
         cur.execute(f"DELETE FROM {table_name} WHERE teams={team};")
-        conn.commit()
-        conn.close()
+        self.conn.commit()
+        # self.conn.close()
 
     def update_info(self, table_name, team, col, value):
-        cur = conn.cursor()
+        cur = self.conn.cursor()
         cur.execute(f"UPDATE {table_name} SET {col}={value} WHERE teams='{team}';")
-        conn.commit()
-        conn.close()
+        self.conn.commit()
+        # self.conn.close()
 
     def show_all(self, table_name):
-        cur = conn.cursor()
+        cur = self.conn.cursor()
         cur.execute(f"SELECT * FROM {table_name};")
         data = cur.fetchall()
-        conn.close()
+        # self.conn.close()
         return data
     
 
 
 
-a = DB()
+# a = DB()
 # a.Create_table(1)
 # a.update_info('league1', 'manchester', 'gf', 10)
 # a.delete_table('league1')

6c84c8751b00093535409fcc0afbc838367ee099
mohsenzolfagharian <mohsen.zolfagharian.98@gmail.com>
2022-04-09 02:38:07 +0430

    changing saved file format to pkl it's easier to work with it

--- read_result_from_saved_file.py
+++ read_result_from_saved_file.py
@@ -1,17 +1,33 @@
 import ast
 import json
 
-# read data from saved file , and it need to get better
+# read data from saved file , and it needed to get better
+import pickle
 
 
 def read():
     data_as_json = {}
     f = open('data.txt', 'r')
     lines = f.readlines()
-    lines = str(lines)
-    data_as_json = json.loads(lines)
-    return data_as_json
+    # print(lines)
+    for line in lines:
+        if line == " ":
+            pass
+        cleaned_line = line.replace("\n", "")
+        print(cleaned_line)
+        data_as_json.update(map(json.loads, cleaned_line))
+
+    # return data_as_json
     # return data_as_json
 
 
-print(read())
+def test_read():
+    file = open("data.pkl", "rb")
+    loaded_data = pickle.load(file)
+    return loaded_data
+
+# read()
+
+print(test_read())
+print(len(test_read()))
+
--- save_matches.py
+++ save_matches.py
@@ -1,19 +1,27 @@
 from Football_data_gen_OOP import League
+from create_league import teams_to_list
+import pickle
 
 
 # create a file to save the result of each match on text file
 def matches_result_save_to_file(list_teams):
     league = League()
-    file = open('data.txt', 'a')
+    data = {}
+    file = open('data.pkl', 'wb')
     for x in range(len(list_teams)):
         c = 1
         while c <= len(list_teams):
             try:
                 goals = league.goals_number()
                 result_match = league.match_result(goals, list_teams[x], list_teams[x + c])
-                file.write(str(result_match) + '\n')
+                # pickle.dump(result_match, file)
+                data.update(result_match)
+                # file.write(result_match)
                 c += 1
             except IndexError:
                 break
-    file.close()
+    # file.close()
+    pickle.dump(data, file)
 
+
+matches_result_save_to_file(teams_to_list())
